dc8de49f45a3e7858cf3a3e4034e928370f65733
wendyladybyui <wendylady_1@byui.edu>
2021-05-15 10:32:45 -0600

    asd

--- Lesson04/js/preston.js
+++ Lesson04/js/preston.js
@@ -1,43 +1,12 @@
+/*document.addEventListener("click", hamburger();
+
+
 function hamburger() {
-    const button = document.querySelector('.hamburger');
-    const nav = document.querySelector('.headermenu')
+    let button = document.querySelector('.hamburger');
+    let nav = document.querySelector('.headermenu');
 
-        button.addEventListener('click', () => {nav.classList.toggle('responsive')}, false);
+	button.addEventListener('click', () => {nav.classList.toggle('responsive')}, false);
 
 
-    window.onresize = () => {if (window.innerWidth > 760) nav.classList.remove('responsive')};
-}
-
-function fulldate() {
-    const daysofweek = [
-        "Sunday",
-         "Monday",
-         "Tuesday",
-        "Wednesday",
-	    "Thursday",
-	    "Friday",
-	    "Saturday"
-];
-    const months = [
-    	"January",
-    	"February",
-     	"March",
-	    "April",
-	    "May",
-	    "June",
-	    "July",
-	    "August",
-	    "September",
-	    "October",
-	    "November",
-	    "December"
-];
-const d = new Date();
-const day = daysofweek[d.getDay()];
-const monthName = months[d.getMonth()];
-const year = d.getFullYear();
-let fulldate = `${day}, ${d.getDate()} ${monthName} ${year}`;
-document.getElementById("currentdate").textContent = fulldate;
-document.getElementById("NewYear").textContent = year;
-}
-
+    window.onresize = () => {if (window.innerWidth > 760px) nav.classList.remove('responsive')}
+}*/
