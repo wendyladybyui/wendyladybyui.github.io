60821cce53b731a8b03e506783e8bf19ffc6d529
wendyladybyui <wendylady_1@byui.edu>
2021-06-28 09:32:48 -0600

    asg

--- Lesson10/js/forecast-api.js
+++ Lesson10/js/forecast-api.js
@@ -17,6 +17,14 @@ fetch(myAPI)
         let d = new Date(x.dt_txt);
         console.log(d);
         document.getElementById(`dayofWeek${day+1}`).textContent = daysofweek[d.getDay()];
+        console.log(`X is: ${x.weather[0].icon}`);
+        const imgsrc = `https://openweathermap.org/img/w/${x.weather[0].icon}.png`;
+        const desc = x.weather[0].description;
+        let image = document.createElement('img');
+        let imageTD = document.getElementById(`icon${day+1}`);
+        image.setAttribute('src', imgsrc);
+        image.setAttribute('alt', desc);
+        imageTD.appendChild(image);
         document.getElementById(`forecast${day+1}`).textContent = x.main.temp_max.toFixed(0);
         document.getElementById(`forecastlo${day+1}`).textContent = x.main.temp_min.toFixed(0);
         document.getElementById(`foredesc${day+1}`).textContent = x.weather[0].description.toUpperCase();
