09e0f5ce521a105047ead94579aef46ff3790d8a
wendyladybyui <wendylady_1@byui.edu>
2021-07-02 16:49:28 -0600

    push

--- Lesson10/js/pancake.js
+++ Lesson10/js/pancake.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-        let day = new Date();
+        
         let dayOptions = {
             weekday: 'long',
             year: 'numeric',
