b0332d3c8cff53c237bb85e6b9261e13f71c5c80
wendyladybyui <wendylady_1@byui.edu>
2021-07-17 16:11:19 -0600

    go

--- lesson13/js/jsoninfo.js
+++ lesson13/js/jsoninfo.js
@@ -9,7 +9,7 @@ fetch(jsonFILE)
   
     let restaurants = jsonObject.businessdirectory[0].restraunts;
 
-  for (let i = 0; i < restaurant.length; i++ ) {
+  for (let i = 0; i < restaurants.length; i++ ) {
 
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
@@ -30,7 +30,29 @@ fetch(jsonFILE)
     card.appendChild(h2).appendChild(img).appendChild(address).appendChild(city).appendChild(phone).appendChild(web);
 
 document.querySelector('div.business-card-restaurant').appendChild(card);
+  }
+let boutiques = jsonObject.businessdirectory[1].boutiques;
 
+  for (let i = 0; i < boutiques.length; i++ ) {
 
+    let boucard = document.createElement('section');
+    let bouh2 = document.createElement('h2');
+    let bouaddress = document.createElement('p');
+    let boucity = document.createElement('p');
+    let bouphone = document.createElement('p');
+    let bouweb = document.createElement('p');
+    let bouimg = document.createElement('img');
+
+    bouh2.textContent = boutiques[i].name;
+    bouimg.src = boutiques[i].logo;
+    bouimg.alt = boutiques[i].name;
+    bouaddress.textContent = boutiques[i].address;
+    boucity.textContent = boutiques[i].city;
+    bouphone.textContent = boutiques[i].phone;
+    bouweb.textContent = boutiques[i].website;
+
+    boucard.appendChild(h2).appendChild(img).appendChild(address).appendChild(city).appendChild(phone).appendChild(web);
+
+document.querySelector('div.business-card-restaurant').appendChild(boucard);
 }
   });
